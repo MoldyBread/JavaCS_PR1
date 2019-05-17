ce493d2d307d4ecfd0c76caefa1ecc79a584f2dc
Dan <lyash.dv@gmail.com>
2019-05-17 11:33:24 +0300

    part without tests

---
+++ src/main/java/com/company/pacakge/PackageManager.java
@@ -0,0 +1,52 @@
+package com.company.pacakge;
+
+import com.company.util.Util;
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+public class PackageManager {
+    private byte[] data;
+    private int encLen;
+
+    public PackageManager(byte[] data) throws InvalidArgumentException {
+        this.data = data;
+        encLen=data.length-16;
+
+        if(!checkPackage()){
+            throw new InvalidArgumentException(new String[]{"",""});
+        }
+    }
+
+    private boolean checkPackage(){
+        System.out.println(Util.CRC(Util.subArray(data,0,13)));
+        System.out.println(sumCRC(14,15));
+        if(getPart(Util.CRC(Util.subArray(data,0,13)),2)!=sumCRC(14,15)){
+            return false;
+        }
+
+        return getPart(Util.CRC(Util.subArray(data, 16, encLen)),data.length - 1-16-encLen)
+                == sumCRC(16 + encLen, data.length - 1);
+    }
+
+    private int sumCRC(int start,int end){
+        int res=0;
+        for (int i = start; i <=end; i++) {
+            res+=data[i];
+        }
+        return res;
+    }
+
+    private int getPart(int number,int count){
+        int res=0;
+        for (int i = 0; i <count ; i++) {
+            if(number<=127){
+                res+=Byte.valueOf(String.valueOf(number));
+                break;
+            }else{
+                res+=127;
+                number-=127;
+            }
+        }
+        return res;
+    }
+
+}
